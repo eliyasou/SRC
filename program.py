4611c410bff17caf34897c9a4ae70ba2d66a1800
elisa <eliaswesth@gmail.com>
2022-12-01 02:36:34 +0100

    bestemt class for planet bestemt class for kanon og bevægelse for kannonen bestemt class for skud samt mulighed for at skyde

--- program.py
+++ program.py
@@ -3,9 +3,17 @@ import math
 import random
 
 
-BREDDE = 800
-HOEJDE = 600
-MOVEMENT_SPEED = 2
+BREDDE = 1000
+HOEJDE = 800
+MOVEMENT_SPEED = 3
+SKUD_HASTIGEDHED = 0
+SPORLAENGDE = 200
+TYNGDEKRAFT = 1
+SPILKØRER = True
+
+
+
+
 class Planet:
     def __init__(self,centrum_x,centrum_y,radius,farve):
         self.centrum_x=centrum_x
@@ -15,7 +23,17 @@ class Planet:
 
     def tegn(self):
         arcade.draw_circle_filled(self.centrum_x,self.centrum_y,self.radius,self.farve)
+class Tyngdefelt:
+    def __init__(self,centrum_x,centrum_y, radius,farve):
+        self.centrum_x=centrum_x
+        self.centrum_y=centrum_y
+        self.radius=radius
+        self.farve=farve
+
+    def tegn(self):
+        arcade.draw_circle_outline(self.centrum_x,self.centrum_y,self.radius,self.farve)
 class Kanon:
+
     def __init__(self,start_x,start_y,slut_x,slut_y,skift_x,skift_y,farve,linje_bredde):
         self.start_x=start_x
         self.start_y=start_y
@@ -33,54 +51,115 @@ class Kanon:
         self.slut_x+=self.skift_x
         self.slut_y+=self.skift_y
 
-        if self.slut_x>BREDDE:
-            self.slut_x=BREDDE
-        if self.slut_x<0:
-            self.slut_x=0
-        if self.slut_y>HOEJDE:
-            self.slut_y=HOEJDE
+        kanonlaengde = math.sqrt((self.slut_x-self.start_x)**2+(self.slut_y-self.start_y)**2)
+
+        if kanonlaengde>=90:
+            print((f"{kanonlaengde}"))
+            pass
 
 
+#        if self.slut_x>BREDDE/4+90:
+ #           self.slut_x=BREDDE/4+90
+  #      if self.slut_x<BREDDE/4-90:
+   #         self.slut_x=BREDDE/4-90
+    #    if self.slut_y>HOEJDE/2+90:
+     #       self.slut_y=HOEJDE/2+90
+      #  if self.slut_y<HOEJDE/2-90:
+       #     self.slut_y=HOEJDE/2-90
+
+class Skud:
+
+    def __init__(self, fast_punkt, retningsvektor, farve, sporlaengde=None):
+        self.fast_punkt = fast_punkt
+        self.retningsvektor = retningsvektor
+        self.punkt = self.fast_punkt
+        self.farve = farve
+        self.sporlaengde = sporlaengde
+        if self.sporlaengde:
+            self.spor = list()
+
+
+    def opdater(self, delta_tid):
+            if self.sporlaengde:
+                self.spor.append(self.punkt)
+            if self.sporlaengde and len(self.spor) >= self.sporlaengde:
+                self.spor.pop(0)
+
+            x, y = self.punkt
+            vx, vy = self.retningsvektor
+            x += vx * delta_tid
+            y += vy * delta_tid
+            self.punkt = (x, y)
+
+    def tegn(self):
+        x, y = self.punkt
+        arcade.draw_circle_filled(x, y, 5, self.farve)
+        for punkt in self.spor:
+            x, y = punkt
+            arcade.draw_circle_filled(x, y, 2, self.farve)
+
+
+    def tegn(self):
+        x, y = self.punkt
+        arcade.draw_circle_filled(x, y, 5, self.farve)
+        for punkt in self.spor:
+            x, y = punkt
+            arcade.draw_circle_filled(x, y, 2, self.farve)
 class MitSpil(arcade.Window):
     def __init__(self,bredde,hoejde,titel):
         super().__init__(bredde,hoejde,titel)
-        arcade.set_background_color(arcade.color.SKY_BLUE)
+        arcade.set_background_color(arcade.color.BLACK)
 
-
-
+    def setup(self):
         self.planet = Planet(BREDDE/4,HOEJDE/2,50,arcade.color.BEAVER)
         self.planet2=Planet(BREDDE/1.25,HOEJDE/2,50,arcade.color.RUBY)
-        self.kanon = Kanon(BREDDE/4,HOEJDE/2,BREDDE/2-100,HOEJDE/2,0,0,arcade.color.GOLD,15)
-
+        self.kanon = Kanon(self.planet.centrum_x,self.planet.centrum_y,self.planet.centrum_x+90,HOEJDE/2,0,0,arcade.color.GOLD,15)
+        self.skud = Skud((self.kanon.start_x,self.kanon.start_y), (0, 0), arcade.csscolor.YELLOW, 100)
+        self.tyngdefelt = Tyngdefelt(self.planet.centrum_x,self.planet.centrum_y,self.planet.radius*4,arcade.color.PLUM)
 
     def on_draw(self):
-        arcade.start_render()
+        self.skud.tegn()
+        self.kanon.tegn()
         self.planet.tegn()
         self.planet2.tegn()
-        self.kanon.tegn()
-    def update(self, delta_time):
-        self.kanon.update()
+        self.tyngdefelt.tegn()
 
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.kanon.skift_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.kanon.skift_x=MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.kanon.skift_y=-MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.kanon.skift_y=MOVEMENT_SPEED
+
+    def on_key_press(self, key, modifiers,):
+        global SPILKØRER
+        if SPILKØRER == True:
+            if key == arcade.key.LEFT:
+                self.kanon.skift_x = -MOVEMENT_SPEED
+            elif key == arcade.key.RIGHT:
+                self.kanon.skift_x=MOVEMENT_SPEED
+            elif key == arcade.key.DOWN:
+                self.kanon.skift_y=-MOVEMENT_SPEED
+            elif key == arcade.key.UP:
+                self.kanon.skift_y=MOVEMENT_SPEED
+            elif key == arcade.key.SPACE:
+                    global SKUD_HASTIGEDHED
+                    self.skud.retningsvektor
+                    self.skud.retningsvektor = self.kanon.slut_x-self.kanon.start_x,self.kanon.slut_y-self.kanon.start_y
+                    SPILKØRER = False
+        elif not SPILKØRER:
+            pass
+
+
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.kanon.skift_x = 0
         elif key == arcade.key.DOWN or key == arcade.key.UP:
             self.kanon.skift_y = 0
-
-
+    def update(self, delta_tid):
+        self.clear()
+        self.kanon.update()
+        self.skud.opdater(delta_tid)
 
 def main():
     vindue = MitSpil(BREDDE,HOEJDE,"Mit Spil")
+    vindue.setup()
     arcade.run()
 
 main()
+
